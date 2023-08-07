9753d06d63b9c12f0c75440203bae20b359e1c8f
butarvds <butarevds@mail.ru>
2023-08-07 15:20:55 +0700

    First HW0 done

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -1,8 +1,10 @@
 public class FirstJavaClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Значение величины - " +i);
+
+        }
 
     }
-
 }
